0a0fa7eba0121931186b02c48369b7351ca69483
James Brooks <jbrooksuk@me.com>
2017-05-06 11:37:32 +0100

    Allow ungrouped components to be collapsed

--- resources/views/partials/modules/components.blade.php
+++ resources/views/partials/modules/components.blade.php
@@ -1,11 +1,12 @@
 @if($component_groups->isNotEmpty() || $ungrouped_components->isNotEmpty())
 <div class="section-components">
-    @if($component_groups->count() > 0)
+    @if($component_groups->isNotEmpty())
     @foreach($component_groups as $componentGroup)
     <ul class="components-group">
-        @if($componentGroup->enabled_components->count() > 0)
+        @if($componentGroup->enabled_components->isNotEmpty())
         <li class="components-group__name group-name">
             <i class="ion ion-ios-circle-filled text-component-{{ $componentGroup->lowest_status }} {{ $componentGroup->lowest_status_color }}" data-toggle="tooltip" title="{{ $componentGroup->lowest_human_status }}"></i>
+
             {{ $componentGroup->name }}
 
             <div class="pull-right">
@@ -21,13 +22,21 @@
     @endforeach
     @endif
 
-    @if($ungrouped_components->count() > 0)
+    @if($ungrouped_components->isNotEmpty())
     <ul class="components-group">
-        <li class="components-group__name">
+        <li class="components-group__name group-name">
             <i class="ion ion-ios-circle-filled text-component-{{ $ungrouped_components->max('status') }}" data-toggle="tooltip" title="{{ $ungrouped_components->sortByDesc('status')->first()->human_status }}"></i>
+
             {{ trans('cachet.components.group.other') }}
+
+            <div class="pull-right">
+                <i class="{{ $componentGroup->collapse_class }} group-toggle"></i>
+            </div>
         </li>
-        @each('partials.component', $ungrouped_components, 'component')
+
+        <div class="group-items">
+            @each('partials.component', $ungrouped_components, 'component')
+        </div>
     </ul>
     @endif
 </div>
