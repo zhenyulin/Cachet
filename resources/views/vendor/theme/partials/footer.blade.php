9d97f3e49ecc165f35acf0a9cc2b7724105e48e4
James Brooks <jbrooksuk@me.com>
2018-06-26 18:53:01 +0100

    Move default theme templates

---
+++ resources/views/vendor/theme/partials/footer.blade.php
@@ -0,0 +1,47 @@
+@if($appFooter)
+{!! $appFooter !!}
+@else
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                @if($showSupport)
+                <p>
+                    {!! trans('cachet.powered_by') !!}
+                    @if($showTimezone)
+                    {{ trans('cachet.timezone', ['timezone' => $timezone]) }}
+                    @endif
+                </p>
+                @endif
+            </div>
+            <div class="col-sm-8">
+                <ul class="list-inline">
+                    @if($currentUser || $dashboardLink)
+                    <li>
+                        <a class="btn btn-link" href="{{ cachet_route('dashboard') }}">{{ trans('dashboard.dashboard') }}</a>
+                    </li>
+                    @endif
+                    @if($currentUser)
+                    <li>
+                        <a class="btn btn-link" href="{{ cachet_route('auth.logout') }}">{{ trans('dashboard.logout') }}</a>
+                    </li>
+                    @endif
+                    <li>
+                        <a class="btn btn-link" href="{{ cachet_route('feed.rss') }}">{{ trans('cachet.rss-feed') }}</a>
+                    </li>
+                    <li>
+                        <a class="btn btn-link" href="{{ cachet_route('feed.atom') }}">{{ trans('cachet.atom-feed') }}</a>
+                    </li>
+                    @if($enableSubscribers)
+                    <li>
+                        <a class="btn btn-success btn-outline" href="{{ cachet_route('subscribe') }}">{{ trans('cachet.subscriber.button') }}</a>
+                    </li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+@endif
+
+@include('theme::partials.analytics')
